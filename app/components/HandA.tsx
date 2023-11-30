e438e87f4526faef52b0e359d75b772a24991f54
dxtobi <akanbijosephtobi@gmail>
2023-11-30 03:07:16 -0800

    carosel fixed

--- app/components/HandA.tsx
+++ app/components/HandA.tsx
@@ -5,80 +5,65 @@ import Image from "next/image";
 import AwardImg from "@/public/assets/awa.png";
 import useFonts from "@/hooks/useFonts";
 
-const awards = [
-  `National Scholarship for Free Secondary Education, Federal Government of Nigeria, 1986`,
-  `Fellow @Institute of Chartered Management Consultants`,
-  `Best graduating Mathematic Senior Secondary School Student, 1992`,
-  `Fellow @Institute of Leadership, Entrepreneurship, and Corporate Governance`,
-  `Management Consultant with Distinction in Time Management (London Graduate School).`,
+const award = [
+  {
+    text: `National Scholarship for Free Secondary Education, Federal Government of Nigeria, 1986`,
+    color: "center",
+    animation: "center",
+  },
+  {
+    text: `Fellow @Institute of Chartered Management Consultants`,
+    color: "left1",
+    animation: "left1",
+  },
+  {
+    text: `Best graduating Mathematic Senior Secondary School Student, 1992`,
+    color: "left",
+    animation: "left",
+  },
+  {
+    text: `Fellow @Institute of Leadership, Entrepreneurship, and Corporate Governance`,
+    color: "right",
+    animation: "right",
+  },
+  {
+    text: `Management Consultant with Distinction in Time Management (London Graduate School).`,
+    color: "right1",
+    animation: "right1",
+  },
 ];
 const HandA = () => {
-  const [positionIndexes, setPositionIndexes] = useState([0, 1, 2, 3, 4]);
+  // [positionIndexes, setPositionIndexes] = useState([0, 1, 2, 3, 4]);
   const [currentIndex, setCurrentIndex] = useState(2);
   const { poppins, libre } = useFonts();
-const [positions, setPositions] = useState(["center", "left1", "left", "right", "right1"])
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     handleDragEnd({}, {offset:{x:300}})
-  //   }, 5000);
+  const [awards, setAward] = useState(award);
+  // const [positions, setPositions] = useState([
+  //   "center",
+  //   "left1",
+  //   "left",
+  //   "right",
+  //   "right1",
+  // ]);
+  
+  const setIndexOnClick = (clickedIndex: number) => {
+    console.log(
+      `INDEX CkD: ${awards[clickedIndex].color},  \nINDEX IN 2:${awards[2].color}, \n`,
+      clickedIndex,
+      currentIndex
+    );
 
-  //   // Clear the interval when the component unmounts or on cleanup
-  //   return () => clearInterval(intervalId);
-  // }, [])
-
-  const handleNext = () => {
-    setPositionIndexes((prevIndexes) => {
-      const updatedIndexes = prevIndexes.map(
-        (prevIndex) => (prevIndex + 1) % 5
-      );
-      
-
-      return updatedIndexes;
-    });
-    
-    const setNewIndex = currentIndex >= 4 ? 1 : currentIndex + 1;
-    setCurrentIndex(setNewIndex);
-  };
-
-  const handleBack = () => {
-    setPositionIndexes((prevIndexes) => {
-      const updatedIndexes = prevIndexes.map(
-        (prevIndex) => (prevIndex + 4) % 5
-      );
-      const setNewIndex = currentIndex <= 0 ? 4 : currentIndex - 1;
-      setCurrentIndex(setNewIndex);
-      return updatedIndexes;
-    });
-  };
-
-  // useEffect(() => {
-  //   console.log( currentIndex)
-  // }, [currentIndex])
-
-  /**
-   * 
-   * @param clickedIndex 
-   */
-  const setIndexOnClick = (clickedIndex:number) => {
-    setPositionIndexes((prevIndexes) => {
-      const newIndexes = [...prevIndexes];
-
-      //console.log(clickedIndex, currentIndex, newIndexes)
-      // Swap clickedIndex with the index at position 0
-      const newIndex = newIndexes[clickedIndex];
-      const current = newIndexes[currentIndex];
-      newIndexes[currentIndex] = newIndex
-      newIndexes[clickedIndex] = current;
-      setCurrentIndex(clickedIndex); 
-      console.log('after', clickedIndex)
-
-      return newIndexes;
-    });
-    
+    const newIndexes = [...awards];
+    const frontIndex = newIndexes.findIndex(item => item.animation === "center")
+    const temp = newIndexes[frontIndex].animation;
+    const click = newIndexes[clickedIndex].animation;
+    newIndexes[frontIndex].animation = click;
+    newIndexes[clickedIndex].animation = temp;
+    setAward(newIndexes);
+    setCurrentIndex(clickedIndex)
    
-  }
+  };
+
  
-  //const colors = ["center", "left1", "left", "right", "right1"];
   const imageVariants = {
     center: { x: "0%", scale: 1, zIndex: 5 },
     left1: { x: "-50%", scale: 0.7, zIndex: 3 },
@@ -86,53 +71,76 @@ const [positions, setPositions] = useState(["center", "left1", "left", "right", 
     right: { x: "90%", scale: 0.5, zIndex: 1 },
     right1: { x: "50%", scale: 0.7, zIndex: 3 },
   };
-  const handleDragEnd = (event: any, info: { offset: { x: number } }) => {
-    const swipeThreshold = 40;
-    if (info?.offset?.x > swipeThreshold) {
-      handleNext();
-    } else if (info?.offset?.x < -swipeThreshold) {
-      handleBack();
-    } else {
-      handleNext();
-    }
-  };
+  // const handleDragEnd = (event: any, info: { offset: { x: number } }) => {
+  //   const swipeThreshold = 40;
+  //   if (info?.offset?.x > swipeThreshold) {
+  //     handleNext();
+  //   } else if (info?.offset?.x < -swipeThreshold) {
+  //     handleBack();
+  //   } else {
+  //     handleNext();
+  //   }
+  // };
+  // const handleNext = () => {
+  //   setPositionIndexes((prevIndexes) => {
+  //     const updatedIndexes = prevIndexes.map(
+  //       (prevIndex) => (prevIndex + 1) % 5
+  //     );
+  //     return updatedIndexes;
+  //   });
+
+  //   const setNewIndex = currentIndex >= 4 ? 1 : currentIndex + 1;
+  //   setCurrentIndex(setNewIndex);
+  // };
+
+  // const handleBack = () => {
+  //   setPositionIndexes((prevIndexes) => {
+  //     const updatedIndexes = prevIndexes.map(
+  //       (prevIndex) => (prevIndex + 4) % 5
+  //     );
+  //     const setNewIndex = currentIndex <= 0 ? 4 : currentIndex - 1;
+  //     setCurrentIndex(setNewIndex);
+  //     return updatedIndexes;
+  //   });
+  // };
+
+ 
   return (
     <div
-      id='handa'
-      className='flex  flex-col gap-10 pb-12 md:px-20 m-auto bg-white overflow-hidden'
+      id="handa"
+      className="flex  flex-col gap-10 pb-12 md:px-20 m-auto bg-white overflow-hidden"
     >
       <h2
         style={libre.style}
-        className='text-center pt-[28px] font-semibold text-[34px]'
+        className="text-center pt-[28px] font-semibold text-[34px]"
       >
         Honors And Awards
       </h2>
-      <div className='flex items-center  sm:mx-5 flex-col justify-around  min-h-[60vh] overflow-hidden w-[95%] mx-auto lg:w-full relative'>
-        {awards.map((text, index) => (
+      <div className="flex items-center  sm:mx-5 flex-col justify-around  min-h-[60vh] overflow-hidden w-[95%] mx-auto lg:w-full relative">
+        {awards.map((comp, index) => (
           <motion.div
             key={index}
-            className={`absolute lg:w-[25%] cursor-pointer w-[80%] rounded-[20px] bg-header-color text-white py-12`}
-            animate={positions[positionIndexes[index]]}
+            className={`absolute lg:w-[25%] cursor-pointer w-[80%] rounded-[20px] bg-header-color text-white py-12 ${comp.color}`}
+            animate={comp.animation}
             variants={imageVariants}
             transition={{ duration: 0.5 }}
-            drag='x'
+            drag="x"
             dragConstraints={{ left: 0, right: 0 }}
-            onDragEnd={handleDragEnd}
-            onClick={()=>setIndexOnClick(index)}
+            onDragEnd={()=>setIndexOnClick(index)}
+            onClick={() => setIndexOnClick(index)}
           >
             <Image
               src={AwardImg}
-              alt=''
-              className='relative bottom-5 lg:w-full'
+              alt=""
+              className="relative bottom-5 lg:w-full"
             />
-            <p style={poppins.style} className='p-3 text-[18px]  font-normal'>
-              --{positions[positionIndexes[index]]}--{positionIndexes[index]}--{currentIndex}--
-              {text}
+            <p style={poppins.style} className="p-3 text-[18px]  font-normal">
+              {comp.text}
             </p>
           </motion.div>
         ))}
       </div>
-      <div className='flex justify-center gap-2 mt-4'>
+      <div className="flex justify-center gap-2 mt-4">
         {awards.map((_, index) => (
           <div
             key={index}
